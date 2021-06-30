a386e1b32284d75eae0cb38346f84658b646f779
StanTheMan7 <sts.graur@gmail.com>
2021-06-30 10:06:24 +0200

    uptating state depending on previous state

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -3,28 +3,73 @@ import './ExpenseForm.css';
 
 
 const ExpenseFrom = () => {
-    // my States   slices good to know that you CAN have multiple states for a single component they will work without affecting the others elements
+    // my States   slices good to know that you CAN have multiple states for a single component they will work without affecting the others states , and in all 3 states we just store some user input  
     const [enteredTitle, setEnteredTitle] = useState('');
     const [enteredAmount, setEnteredAmount] = useState('');
     const [enteredDate, setEnteredDate] = useState('');
+
+    
+// insted of 3 states we can go for one sate insted, by calling used State only onece and by passing in an object as a value and now you can group together your 3 states 
+    // const [userInput , setUserInput] = useState({
+    //     enteredTitle:'',
+    //     enteredAmount: '',
+    //     enteredDate: '' 
+    // })
+
+
 
 
     const titleChangeHandler = (event) =>{
         // now we have to think how to get the value that we type in our input
         setEnteredTitle(event.target.value);
+
+        // setUserInput({
+            // ... is a default JavaScript Operator in modern js and you copy in the input, this neon just takes an object pulls out all the key value pairs  ,and adds them to this new object
+            // ...userInput,
+            // enteredTitle: event.target.value, 
+            // enteredAmount: '',
+            // enteredDate: ''
+        // })
+        //  if your state depends n the previous state use this function form 
+        // setUserInput((prevState) => {
+        //     return {...prevState,  enteredTitle: event.target.value}
+        // });
     }
     
+
+
     const amountChangeHandler = (event) => {
+
         setEnteredAmount(event.target.value);
+
+        // setUserInput({
+            // ... is a default JavaScript Operator in modern js and you copy in the input, this neon just takes an object pulls out all the key value pairs  ,and adds them to this new object
+        //     ...userInput,
+        // enteredAmount : event.target.value, 
+        //     enteredAmount: '',
+        //     enteredDate: ''
+        // })
+
     }
+
+
 
     const dateChangeHandler = (event) => {
         setEnteredDate(event.target.value)
+        // setUserInput({
+            // ... is a default JavaScript Operator in modern js and you copy in the input, this neon just takes an object pulls out all the key value pairs  ,and adds them to this new object
+        //     ...userInput,
+        //     enteredDate: event.target.value, 
+        //     enteredAmount: '',
+        //     enteredDate: ''
+        // })
     }
-
+    const submitHandler = (event) => {
+        event.preventDefault();
+    }
 return (
 
-<form>
+<form onSubmit={submitHandler}>
     <div className="new-expense__controls">
         <div className="new-expense__control">
             <label>Title </label>
